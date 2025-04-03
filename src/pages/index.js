4d92d7dfbf7c049fcdc020484762acca4cad8352
Paulo352 <paulocauan39@gmail.com>
2025-04-03 04:35:18 +0000

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import { useAuth } from '../contexts/AuthContext';
 import BallotBox from '../components/BallotBox';
 import AdminPanel from '../components/AdminPanel';
-import login from '../auth/login';
+import login from '../pages/auth/login';
 
 export default function Home() {
   const { currentUser, hasRole } = useAuth();
